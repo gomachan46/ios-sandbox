216d2c89c9f78c1966681b73601d27091bf05ca5
gomachan46 <shiro.gomachan46@gmail.com>
2019-02-14 14:13:54 +0900

    ひとまずpinchしつつpanできるようになった

--- ios-sandbox/ItemViewController.swift
+++ ios-sandbox/ItemViewController.swift
@@ -12,6 +12,10 @@ class ItemViewController: UIViewController {
     private let disposeBag = DisposeBag()
     private let imageTag = 1
     private var backgroundAlpha: CGFloat = 1.0
+    private var currentTransform: CGAffineTransform!
+    private var viewCenter: CGPoint!
+    private var pinchCenter: CGPoint!
+    private var isZooming: Bool = false
 
     init(item i: Item) {
         item.accept(i)
@@ -78,34 +82,67 @@ class ItemViewController: UIViewController {
                 }
                 this.showsHorizontalScrollIndicator = false
                 this.showsVerticalScrollIndicator = false
-                this.addGestureRecognizer(UIPinchGestureRecognizer(target: self, action: #selector(self.pinchGesture(_:))))
             }
             UIImageView().apply { this in
                 pageScrollView.addSubview(this)
                 this.tag = imageTag
                 this.snp.makeConstraints { make in
-                    make.size.equalTo(scrollView)
+                    make.size.equalTo(pageScrollView)
                 }
                 this.contentMode = .scaleAspectFit
                 item.asDriver().drive(onNext: { item in
                         this.kf.setImage(with: URL(string: "https://picsum.photos/300?image=\(Int.random(in: 1...100))"))
                     })
                     .disposed(by: disposeBag)
+                this.isUserInteractionEnabled = true
+                let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(self.pinchGesture(_:)))
+                pinchGesture.delegate = self
+                this.addGestureRecognizer(pinchGesture)
+                let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.panGesture(_:)))
+                panGesture.minimumNumberOfTouches = 1
+                panGesture.maximumNumberOfTouches = 2
+                panGesture.delegate = self
+                this.addGestureRecognizer(panGesture)
             }
         }
     }
 }
 
 extension ItemViewController {
+    @objc func panGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
+        guard isZooming else { return }
+        guard let zoomingView = gestureRecognizer.view else { return }
+
+        let transform = zoomingView.transform
+        zoomingView.transform = CGAffineTransform.identity
+
+        let point = gestureRecognizer.translation(in: zoomingView)
+        let movedPoint = CGPoint(x: zoomingView.center.x + point.x, y: zoomingView.center.y + point.y)
+        zoomingView.center = movedPoint
+        zoomingView.transform = transform
+    }
+
     @objc func pinchGesture(_ gestureRecognizer: UIPinchGestureRecognizer) {
         guard let zoomingView = gestureRecognizer.view else { return }
+
         switch gestureRecognizer.state {
-        case .began, .changed:
-            zoomingView.transform = zoomingView.transform.scaledBy(x: gestureRecognizer.scale, y: gestureRecognizer.scale)
+        case .began:
             scrollView.snp.removeConstraints()
             scrollView.snp.makeConstraints { make in
                 make.size.edges.equalTo(view)
             }
+            currentTransform = zoomingView.transform
+            viewCenter = zoomingView.center
+            let touchPoint1 = gestureRecognizer.location(ofTouch: 0, in: view)
+            let touchPoint2 = gestureRecognizer.location(ofTouch: 1, in: view)
+            pinchCenter = CGPoint(x: (touchPoint1.x + touchPoint2.x) / 2, y: (touchPoint1.y + touchPoint2.y) / 2)
+            isZooming = true
+        case .changed:
+            let scale = gestureRecognizer.scale
+            let newCenter = CGPoint(x: viewCenter.x - ((pinchCenter.x - viewCenter.x) * scale - (pinchCenter.x - viewCenter.x)), y: viewCenter.y - ((pinchCenter.y - viewCenter.y) * scale - (pinchCenter.y - viewCenter.y)))
+
+            zoomingView.center = newCenter
+            zoomingView.transform = currentTransform.concatenating(CGAffineTransform(scaleX: scale, y: scale))
             view.isOpaque = false
             backgroundAlpha = backgroundAlpha / gestureRecognizer.scale
             if backgroundAlpha > 1.0 {
@@ -126,10 +163,10 @@ extension ItemViewController {
                 self.view.backgroundColor = .white
                 zoomingView.transform = CGAffineTransform.identity
             })
+            isZooming = false
         default:
             break
         }
-        gestureRecognizer.scale = 1.0
     }
 }
 extension ItemViewController: UIScrollViewDelegate {
@@ -138,5 +175,11 @@ extension ItemViewController: UIScrollViewDelegate {
             // 本当はdelegateごと分けたほうが良さそう
             pageView.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
         }
+    }
+}
+
+extension ItemViewController: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
