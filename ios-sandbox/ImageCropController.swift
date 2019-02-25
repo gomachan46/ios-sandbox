b2468fadf5cfafa702fa765a5ef063593208f5dd
gomachan46 <shiro.gomachan46@gmail.com>
2019-02-25 20:18:28 +0900

    キャプションをつけれるようにした

--- ios-sandbox/ImageCropController.swift
+++ ios-sandbox/ImageCropController.swift
@@ -6,6 +6,7 @@ import Photos
 
 class ImageCropController: UIViewController {
     private var image: UIImage!
+    private let disposeBag = DisposeBag()
 
     init(image i: UIImage) {
         image = i
@@ -19,12 +20,66 @@ class ImageCropController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        UIImageView(image: image).apply { this in
+        let stackView = UIStackView().apply { this in
             view.addSubview(this)
+            this.axis = .vertical
+            this.alignment = .center
+            this.distribution = .equalSpacing
+            this.spacing = 10
             this.snp.makeConstraints { make in
-                make.top.equalTo(view.safeAreaLayoutGuide)
-                make.left.right.equalTo(view)
+                make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+                make.left.equalTo(view).offset(10)
+                make.right.equalTo(view).offset(-10)
             }
         }
+        UIImageView(image: image).apply { this in
+            stackView.addArrangedSubview(this)
+            this.snp.makeConstraints { make in
+                make.width.equalTo(image.size.width / 2)
+                make.height.equalTo(image.size.height / 2)
+            }
+        }
+        UIView().apply { this in
+            stackView.addArrangedSubview(this)
+            this.backgroundColor = .lightGray
+            this.snp.makeConstraints { make in
+                make.width.equalTo(stackView)
+                make.height.equalTo(1)
+            }
+        }
+        let caption = UITextView().apply { this in
+            stackView.addArrangedSubview(this)
+            this.snp.makeConstraints { make in
+                make.width.equalTo(stackView)
+                make.height.equalTo(view).dividedBy(5)
+            }
+            this.textColor = .black
+        }
+        UILabel().apply { this in
+            caption.addSubview(this)
+            this.text = "キャプションを書く"
+            this.font = caption.font
+            this.textColor = .lightGray
+            this.snp.makeConstraints { make in
+                make.top.left.equalTo(caption).inset(5)
+            }
+            caption.rx.text.asObservable()
+                .map { $0?.count ?? 0 > 0 }
+                .bind(to: this.rx.isHidden)
+                .disposed(by: disposeBag)
+        }
+        UIView().apply { this in
+            stackView.addArrangedSubview(this)
+            this.backgroundColor = .lightGray
+            this.snp.makeConstraints { make in
+                make.width.equalTo(stackView)
+                make.height.equalTo(1)
+            }
+        }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
 }
