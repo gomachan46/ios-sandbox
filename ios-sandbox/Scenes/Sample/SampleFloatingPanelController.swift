b76dc96c848731d5eef3a492bb1d3eda21468d3e
gomachan46 <shiro.gomachan46@gmail.com>
2019-03-25 13:45:54 +0900

    delegate実装

--- ios-sandbox/Scenes/Sample/SampleFloatingPanelController.swift
+++ ios-sandbox/Scenes/Sample/SampleFloatingPanelController.swift
@@ -18,7 +18,38 @@ class SampleFloatingPanelController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         fpc = FloatingPanelController()
+        fpc.delegate = self
         fpc.set(contentViewController: contentViewController)
         fpc.addPanel(toParent: self)
     }
+}
+
+extension SampleFloatingPanelController: FloatingPanelControllerDelegate {
+    public func floatingPanel(_ vc: FloatingPanelController, layoutFor newCollection: UITraitCollection) -> FloatingPanelLayout? {
+        return SampleFloatingPanelControllerLayout()
+    }
+
+    public func floatingPanel(_ vc: FloatingPanelController, behaviorFor newCollection: UITraitCollection) -> FloatingPanelBehavior? {
+        return SelectUploadImageFloatingPanelBehavior()
+    }
+}
+
+class SampleFloatingPanelControllerLayout: FloatingPanelLayout {
+    var initialPosition: FloatingPanelPosition {
+        return .tip
+    }
+    var supportedPositions: Set<FloatingPanelPosition> {
+        return [.full, .tip]
+    }
+
+    func insetFor(position: FloatingPanelPosition) -> CGFloat? {
+        switch position {
+        case .full: return 56.0
+        case .tip: return 200.0
+        default: return nil
+        }
+    }
+}
+
+class SampleFloatingPanelControllerBehavior: FloatingPanelBehavior {
 }
